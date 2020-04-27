974f3f7cc2184329ced36d8edaacb3d1e7ee59bf
Harley McDowell <hmcdowell95@gmail.com>
2020-04-27 20:03:24 +0000

    Automatically backed up by Learn

--- db/migrate/001.rb
+++ db/migrate/001.rb
@@ -1,3 +1,10 @@
 class CreateTable < ActiveRecord::Migration[5.2]
   
-  
+  def change
+    create_table :users do |x|
+      x.string :name 
+      x.string :email 
+      x.string :password 
+    end
+  end
+end
